b16b862f6d083b344df553dec382b9aafc3523e5
Teszt Elek <you@inf.unideb.hu>
2024-11-18 11:40:14 +0100

    Table in html shown

--- src/main/java/hu/unideb/inf/bootstrap/controller/MainController.java
+++ src/main/java/hu/unideb/inf/bootstrap/controller/MainController.java
@@ -1,12 +1,28 @@
 package hu.unideb.inf.bootstrap.controller;
 
+import hu.unideb.inf.bootstrap.model.Person;
+import hu.unideb.inf.bootstrap.model.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class MainController {
+    @Autowired
+    PersonRepository personRepository;
+
     @GetMapping("")
     public String showHomePage(){
         return "index";
+    }
+
+    @GetMapping("/persons")
+    public String getAllPersons(Model model){
+        List<Person> personList = personRepository.findAll();
+        model.addAttribute("personList", personList);
+        return "persons";
     }
 }
